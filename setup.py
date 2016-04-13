3a8f2d6d25e03758b6b5bdc7a0e44ff2ef45670f
Piotr Szymanski <piotr.szymanski@fieldaware.com>
2016-04-13 12:57:11 +0200

    add first requirement to the package (click, cli tool)

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
+read_file = lambda x: [l.strip() for l in open(x).readlines()]
 
 setup(
     name='confgen',
     version='0.1.0',
     packages=find_packages(),
-    scripts=["bin/confgen"]
+    scripts=["bin/confgen"],
+    install_requires=read_file("requirements.txt"),
 )
