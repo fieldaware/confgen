4dfaf46439fdea90bf79265facf4cad789cdd4c9
Piotr Szymanski <piotr.szymanski@fieldaware.com>
2016-04-13 16:14:33 +0200

    basic commands

--- confgen/cli.py
+++ confgen/cli.py
@@ -1,5 +1,44 @@
-def main():
-    print("czesc")
+import click
+
+@click.group()
+def cli():
+    pass
+
+@cli.group()
+def search():
+    pass
+
+
+@search.command()
+@click.argument('pattern')
+def key(pattern):
+    click.echo("will search key " + pattern)
+
+@search.command()
+@click.argument('pattern')
+def value(pattern):
+    click.echo("will search value " + pattern)
+
+@cli.command()
+def build():
+    click.echo("will build the templates")
+
+@cli.command()
+@click.argument('path')
+@click.argument('key')
+@click.argument('value')
+def set(path, key, value):
+    click.echo("will set the value: {}, at key: {} and path:{}".format(value, key, path))
+
+@cli.command()
+@click.argument('path')
+@click.argument('key')
+def delete(path, key):
+    click.echo("will delete the key: {} at path: {}".format(path, key))
+
+@cli.command()
+def lint():
+    click.echo("will lint templates")
 
 if __name__ == "__main__":
-    main()
+    cli()
