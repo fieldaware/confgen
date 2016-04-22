1054e0f7a87fde061318aab5dbd4255e2faacdff
Piotr Szymanski <piotr.szymanski@fieldaware.com>
2016-04-22 14:32:08 +0200

    set version to 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ read_file = lambda x: [l.strip() for l in open(x).readlines()]
 
 setup(
     name='confgen',
-    version='1.0.0',
+    version='0.1.0',
     packages=find_packages(),
     scripts=["bin/confgen"],
     install_requires=read_file("requirements.txt"),
