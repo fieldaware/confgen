a3e41de997f2c23afb53170afae91654d328a2a0
Piotr Szymanski <piotr.szymanski@fieldaware.com>
2016-05-17 14:09:37 +0200

    Inventory refactoring p1

--- confgen/inventory.py
+++ confgen/inventory.py
@@ -22,6 +22,16 @@ class Inventory(object):
         self.home = home
         self.inventory_dir = join(home, 'inventory')
 
+    def _parse_file_path(self, path):
+        '''
+        takes absolute path in inventory and parses it relative
+        '''
+        relative_path = path[len(self.inventory_dir):]  # strip absolute path
+        if relative_path == '':  # set root path as '/'
+            return '/'
+
+        return relative_path
+
     def collect(self):
         '''
         walks to home dir and collects yaml files
@@ -30,7 +40,7 @@ class Inventory(object):
         for path, dirs, files in os.walk(self.inventory_dir):
             if files:
                 configyml = yaml.load(open(join(path, self.config_filename)))
-                inventory['/' + path[len(self.inventory_dir) + 1:]] = configyml
+                inventory[self._parse_file_path(path)] = configyml
         return inventory
 
     def set(self, path, key, value):
@@ -38,7 +48,7 @@ class Inventory(object):
         if path not in collected:
             collected[path] = {}
         collected[path][key] = value
-        self.flush(collected)
+        self._flush(collected)
 
     def delete(self, path, key):
         collected = self.collect()
@@ -46,37 +56,36 @@ class Inventory(object):
             return
         if key in collected[path]:
             collected[path].pop(key)
-        self.flush(collected)
+        self._flush(collected)
 
-    def flush(self, inventory):
+    def _flush(self, inventory):
         for path, contents in inventory.items():
             dst_dir = join(self.inventory_dir, path.strip('/'))
             mkdir_p(dst_dir)
             with open(join(dst_dir, self.config_filename), 'w+') as f:
                 yaml.dump(contents, f, default_flow_style=False)
 
+    def _build_single_row(self, inventory, path):
+        list_path = ['/'] if path == '/' else ['/'] + path.split('/')[1:]
+        kv_set = {}
+        sources = {}
+        for i, _ in enumerate(list_path, start=1):
+            update_with_path = '/{}'.format('/'.join(list_path[1:i]))
+            update_with = inventory.get(update_with_path, {})
+            for k in update_with:
+                sources.setdefault(k, []).append(update_with_path)
+            kv_set.update(update_with)
+        prefixed_source_vars = {
+            Inventory.source_key_pattern.format(key=k):
+                '{key}:{path}'.format(key=k, path=v[0]) + (' override:{paths}'.format(paths=','.join(v[1:])) if v[1:] else '')
+            for k, v in sources.items()
+        }
+        kv_set.update(prefixed_source_vars)
+        return kv_set
+
     def build(self):
         '''
         builds flat dict structure based on loaded files
         '''
         inventory = self.collect()
-        public_inventory = {}
-
-        for path in sorted(inventory.keys()):
-            list_path = ['/'] if path == '/' else ['/'] + path.split('/')[1:]
-            kv_set = {}
-            sources = {}
-            for i, _ in enumerate(list_path, start=1):
-                update_with_path = '/{}'.format('/'.join(list_path[1:i]))
-                update_with = inventory.get(update_with_path, {})
-                for k in update_with:
-                    sources.setdefault(k, []).append(update_with_path)
-                kv_set.update(update_with)
-            prefixed_source_vars = {
-                Inventory.source_key_pattern.format(key=k):
-                    '{key}:{path}'.format(key=k, path=v[0]) + (' override:{paths}'.format(paths=','.join(v[1:])) if v[1:] else '')
-                for k, v in sources.items()
-            }
-            kv_set.update(prefixed_source_vars)
-            public_inventory[path] = kv_set
-        return public_inventory
+        return {path: self._build_single_row(inventory, path) for path in sorted(inventory.keys())}
