d0aad5795d1316398038273aa5bd94871250591e
Piotr Szymanski <piotr.szymanski@fieldaware.com>
2016-05-19 09:09:24 +0200

    bump to 0.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ read_file = lambda x: [l.strip() for l in open(x).readlines()]
 
 setup(
     name='confgen',
-    version='0.4.1',
+    version='0.5.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=read_file("requirements.txt"),
