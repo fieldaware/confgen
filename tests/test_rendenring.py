2a56cad77433c6a560c65a2fe851e07e5eae461e
Piotr Szymanski <piotr.szymanski@fieldaware.com>
2017-09-13 22:19:22 +0200

    working node

--- tests/test_rendenring.py
+++ tests/test_rendenring.py
@@ -9,21 +9,21 @@ def test_collect_templates(renderer):
 def test_render_templates_for_given_service_and_inventory(renderer, confgen):
     built_inventory = confgen.merge_config_with_inventory()
 
-    templates = renderer.render_templates_for_service('webapp', built_inventory['/prod/main/webapp1'].all_attrs)
+    templates = renderer.render_templates_for_service('webapp', built_inventory['/prod/main/webapp1'])
 
     assert templates == {
         'webapp/my.cnf': u'# mysql:/ override: /prod, /prod/main\n# secret:/\nconnurl = 3.0:password',
         'webapp/production.ini': u'# mysql:/ override: /prod, /prod/main\nmysql = 3.0\n\n# secret:/\npassword = password',
     }
 
-    templates = renderer.render_templates_for_service('webapp', built_inventory['/dev/qa1'].all_attrs)
+    templates = renderer.render_templates_for_service('webapp', built_inventory['/dev/qa1'])
 
     assert templates == {
         'webapp/my.cnf': u'# mysql:/ override: /dev/qa1\n# secret:/\nconnurl = 4.0:password',
         'webapp/production.ini': u'# mysql:/ override: /dev/qa1\nmysql = 4.0\n\n# secret:/\npassword = password',
     }
 
-    templates = renderer.render_templates_for_service('api', built_inventory['/test'].all_attrs)
+    templates = renderer.render_templates_for_service('api', built_inventory['/test'])
 
     assert templates == {
         'api/my.cnf': u'# mysql:/\n# secret:/ override: /test\nconnurl = 1.0:plaintext',
@@ -34,7 +34,7 @@ def test_render_templates_for_multiple_service_and_inventory(renderer, confgen):
 
     templates = renderer.render_templates_for_services(
         ['webapp', 'api'],
-        built_inventory['/prod/main/webapp1'].all_attrs
+        built_inventory['/prod/main/webapp1']
     )
 
     assert templates == {
@@ -46,7 +46,7 @@ def test_render_templates_for_multiple_service_and_inventory(renderer, confgen):
 def test_render_template_with_undefined_key(renderer, confgen):
     built_inventory = confgen.merge_config_with_inventory()
 
-    built_inventory['/prod/main/webapp1'].all_attrs.pop('secret')  # remove required key to render
+    built_inventory['/prod/main/webapp1'].pop('secret')  # remove required key to render
 
     with pytest.raises(SystemExit):
         renderer.render_templates_for_service('webapp', built_inventory['/prod/main/webapp1'])
